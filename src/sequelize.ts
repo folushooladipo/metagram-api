6773a5fe697a4e80f4ac2709c42a3719931145a6
Folusho Oladipo <fdoladipo@gmail.com>
2021-09-01 19:23:53 +0100

    Enable connecting to a local database

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -5,18 +5,23 @@ import {config} from "./config/config"
 
 const devConfig = config.dev
 
+const isLocalEnv = process.env.NODE_ENV === "local"
+const dialectOptions = isLocalEnv
+  ? {}
+  : {
+    ssl: {
+      require: true,
+      ca: readFileSync(`${__dirname}/us-east-1-bundle.pem`),
+    },
+  }
+
 export const sequelize = new Sequelize({
   username: devConfig.username,
   password: devConfig.password,
   database: devConfig.database,
   host: devConfig.host,
   dialect: "postgres",
-  ssl: true,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      ca: readFileSync(`${__dirname}/us-east-1-bundle.pem`),
-    },
-  },
+  ssl: isLocalEnv ? false : true,
+  dialectOptions,
   logging: false,
 })
